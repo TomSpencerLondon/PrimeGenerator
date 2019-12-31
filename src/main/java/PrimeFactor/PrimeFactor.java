b33ef2fc19494c09d573c1c40fc76a9d0215ea5b
Tom Spencer <tom@homeflow.co.uk>
2019-12-31 23:39:45 +0000

    All tests passing for first three numberS

--- src/main/java/PrimeFactor/PrimeFactor.java
+++ src/main/java/PrimeFactor/PrimeFactor.java
@@ -6,8 +6,8 @@ public class PrimeFactor {
     public static ArrayList<Integer> of(int n){
         ArrayList<Integer> factors;
         factors = new ArrayList<>();
-        if(n == 2){
-            factors.add(2);
+        if(n > 1){
+            factors.add(n);
         }
         return factors;
     }
